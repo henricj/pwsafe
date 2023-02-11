ab6cb0829ea48e527611cb933cf32dacf61bf38a
Henric Jungheim <software@henric.org>
2023-02-11 00:07:29 -0700

    Get CMake Ninja and Xcode builds working.

---
+++ CMakeModules/set-options-macos.cmake
@@ -0,0 +1,9 @@
+if (NOT APPLE)
+  return()
+endif ()
+
+#set(MACOSX_BUNDLE_COPYRIGHT "Copyright © 2003-2023 Rony Shapiro All rights reserved")
+#set(MACOSX_BUNDLE_GUI_IDENTIFIER "org.pwsafe.$${PRODUCT_NAME:rfc1034identifier}")
+#set(MACOSX_BUNDLE_ICON_FILE "pwsafe.icns")
+#set(MACOSX_BUNDLE_SHORT_VERSION_STRING "1.0")
+
